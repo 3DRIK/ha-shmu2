89cc039287edadae4d3dc59a4d30ba1ed8b179a7
3DRIK <96953423+3DRIK@users.noreply.github.com>
2026-02-27 22:37:58 +0100

    Update sensor.py

--- custom_components/shmu/sensor.py
+++ custom_components/shmu/sensor.py
@@ -5,6 +5,75 @@ from homeassistant.helpers.entity import DeviceInfo
 from datetime import datetime, timedelta
 from .const import DOMAIN
 
+class SHMUSensor(CoordinatorEntity, SensorEntity):
+    """Representation of a SHMU sensor."""
+
+    def __init__(
+        self,
+        coordinator,
+        sensor_key: str,
+        name: str,
+        unit: str,
+        device_class: SensorDeviceClass = None,
+        icon: str = None,
+        state_class: SensorStateClass = None,
+    ):
+        """Initialize the sensor."""
+        super().__init__(coordinator)
+        self._sensor_key = sensor_key
+        self._name = name
+        self._unit = unit
+        self._device_class = device_class
+        self._icon = icon
+        self._state_class = state_class
+        self._attr_unique_id = f"{DOMAIN}_{coordinator.config_entry.entry_id}_{sensor_key}"
+
+        # Dynamic device name based on station_id
+        station_id = coordinator.config_entry.data["station_id"]
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, coordinator.config_entry.entry_id)},
+            name=f"SHMU Station {station_id}",
+            manufacturer="Slovenský hydrometeorologický ústav",
+            model="AWS Weather Station",
+            sw_version="1.0",
+        )
+
+    @property
+    def unique_id(self) -> str:
+        """Return the unique ID of the sensor."""
+        return self._attr_unique_id
+
+    @property
+    def name(self) -> str:
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def native_value(self):
+        """Return the state of the sensor."""
+        return self.coordinator.data.get(self._sensor_key)
+
+    @property
+    def native_unit_of_measurement(self) -> str:
+        """Return the unit of measurement as a string."""
+        return self._unit
+
+    @property
+    def device_class(self) -> SensorDeviceClass:
+        """Return the device class."""
+        return self._device_class
+
+    @property
+    def state_class(self) -> SensorStateClass:
+        """Return the state class."""
+        return self._state_class
+
+    @property
+    def icon(self) -> str:
+        """Return the icon."""
+        return self._icon
+
+
 class SHMUMeteogramSensor(CoordinatorEntity, SensorEntity):
     """Representation of a SHMU meteogram URL sensor."""
 
