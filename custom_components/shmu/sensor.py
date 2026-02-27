7dd89f2cfad5a2e589be8446f30ac8475167bd34
3DRIK <96953423+3DRIK@users.noreply.github.com>
2026-02-27 16:30:39 +0100

    Update sensor.py

--- custom_components/shmu/sensor.py
+++ custom_components/shmu/sensor.py
@@ -1,47 +1,111 @@
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
-from homeassistant.const import TEMP_CELSIUS, PERCENTAGE, PRESSURE_HPA, SPEED_METERS_PER_SECOND, DEGREE
+from homeassistant.const import (
+    TEMP_CELSIUS,
+    PERCENTAGE,
+    UnitOfPressure,
+    UnitOfSpeed,
+    UnitOfTemperature,
+)
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers.typing import UnitType
 from .const import DOMAIN
 
 class SHMUSensor(CoordinatorEntity, SensorEntity):
-    """Base class for SHMU sensors."""
+    """Representation of a SHMU sensor."""
 
-    def __init__(self, coordinator, sensor_key, name, unit, device_class=None):
+    def __init__(
+        self,
+        coordinator,
+        sensor_key: str,
+        name: str,
+        unit: UnitType,
+        device_class: SensorDeviceClass = None,
+        icon: str = None,
+    ):
+        """Initialize the sensor."""
         super().__init__(coordinator)
         self._sensor_key = sensor_key
         self._name = name
         self._unit = unit
         self._device_class = device_class
-        self._unique_id = f"{DOMAIN}_{coordinator.config_entry.entry_id}_{sensor_key}"
+        self._icon = icon
+        self._attr_unique_id = f"{DOMAIN}_{coordinator.config_entry.entry_id}_{sensor_key}"
 
     @property
-    def unique_id(self):
-        return self._unique_id
+    def unique_id(self) -> str:
+        """Return the unique ID of the sensor."""
+        return self._attr_unique_id
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """Return the name of the sensor."""
         return self._name
 
     @property
-    def state(self):
+    def native_value(self):
+        """Return the state of the sensor."""
         return self.coordinator.data.get(self._sensor_key)
 
     @property
-    def unit_of_measurement(self):
+    def native_unit_of_measurement(self) -> UnitType:
+        """Return the unit of measurement."""
         return self._unit
 
     @property
-    def device_class(self):
+    def device_class(self) -> SensorDeviceClass:
+        """Return the device class."""
         return self._device_class
 
+    @property
+    def icon(self) -> str:
+        """Return the icon."""
+        return self._icon
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up sensors for SHMU integration."""
+    """Set up the SHMU sensors."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
+
     sensors = [
-        SHMUSensor(coordinator, "t", "Temperature", TEMP_CELSIUS, SensorDeviceClass.TEMPERATURE),
-        SHMUSensor(coordinator, "vlh_rel", "Humidity", PERCENTAGE, SensorDeviceClass.HUMIDITY),
-        SHMUSensor(coordinator, "tlak", "Pressure", PRESSURE_HPA, SensorDeviceClass.PRESSURE),
-        SHMUSensor(coordinator, "vie_pr_rych", "Wind Speed", SPEED_METERS_PER_SECOND),
-        SHMUSensor(coordinator, "vie_pr_smer", "Wind Direction", DEGREE)
+        SHMUSensor(
+            coordinator=coordinator,
+            sensor_key="t",
+            name="SHMU Temperature",
+            unit=UnitOfTemperature.CELSIUS,
+            device_class=SensorDeviceClass.TEMPERATURE,
+            icon="mdi:thermometer",
+        ),
+        SHMUSensor(
+            coordinator=coordinator,
+            sensor_key="vlh_rel",
+            name="SHMU Humidity",
+            unit=PERCENTAGE,
+            device_class=SensorDeviceClass.HUMIDITY,
+            icon="mdi:water-percent",
+        ),
+        SHMUSensor(
+            coordinator=coordinator,
+            sensor_key="tlak",
+            name="SHMU Pressure",
+            unit=UnitOfPressure.HPA,
+            device_class=SensorDeviceClass.PRESSURE,
+            icon="mdi:gauge",
+        ),
+        SHMUSensor(
+            coordinator=coordinator,
+            sensor_key="vie_pr_rych",
+            name="SHMU Wind Speed",
+            unit=UnitOfSpeed.METERS_PER_SECOND,
+            device_class=SensorDeviceClass.WIND_SPEED,
+            icon="mdi:weather-windy",
+        ),
+        SHMUSensor(
+            coordinator=coordinator,
+            sensor_key="vie_pr_smer",
+            name="SHMU Wind Direction",
+            unit=UnitType.DEGREE,
+            device_class=SensorDeviceClass.WIND_SPEED,
+            icon="mdi:compass",
+        ),
     ]
+
     async_add_entities(sensors)
