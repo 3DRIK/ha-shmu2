dce0602b28998a48f759914a3fe376f1a3107cc0
3DRIK <96953423+3DRIK@users.noreply.github.com>
2026-02-28 09:29:29 +0100

    Update __init__.py

--- custom_components/shmu/__init__.py
+++ custom_components/shmu/__init__.py
@@ -20,13 +20,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = {"coordinator": coordinator}
 
     # Forward setup to sensor and camera platforms
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor","camera"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
     
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, ["sensor","camera"]):
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, ["sensor"]):
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
 
